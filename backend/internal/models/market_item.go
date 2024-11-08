ea99072e8e050e2c0755e6011c3df3f288219f57
joaoferreiravnf <joaoferreira@C1F4H7X1X9.local>
2024-11-08 15:49:21 +0000

    changing to pure html and css on the frontend, adding createItem and updating listItem

--- backend/internal/models/market_item.go
+++ backend/internal/models/market_item.go
@@ -2,22 +2,39 @@ package models
 
 import (
 	"github.com/pkg/errors"
+	"slices"
 	"strings"
 	"time"
 	"unicode"
+)
+
+const (
+	Continente = "Continente"
+	Lidl       = "Lidl"
+	Belita     = "Belita"
+	PingoDoce  = "Pingo Doce"
+	Jumbo      = "Jumbo"
+)
+
+const (
+	Fruits      = "Frutas"
+	Vegetables  = "Legumes"
+	MeatAndFish = "Carne & Peixe"
+	Drinks      = "Bebidas"
+	Higiene     = "Higiene"
 )
 
 // TODO: Consider imterface implementation for future structs (multiuse app)
 
 // Item is the struct for the market item
 type Item struct {
-	ID       int       `db:"id"`
-	Name     string    `db:"name"`
-	Quantity int       `db:"qty"`
-	Type     string    `db:"type"`
-	Market   string    `db:"market"`
-	AddedAt  time.Time `db:"added_at"`
-	AddedBy  string    `db:"added_by"`
+	ID       int    `db:"id"`
+	Name     string `db:"name"`
+	Quantity int    `db:"qty"`
+	Type     string `db:"type"`
+	Market   string `db:"market"`
+	AddedAt  string `db:"added_at"`
+	AddedBy  string `db:"added_by"`
 }
 
 func (i *Item) NormalizeFieldsForPersistence() error {
@@ -42,9 +59,7 @@ func (i *Item) NormalizeFieldsForPersistence() error {
 		return errors.Wrap(err, "unable to normalize 'market' field")
 	}
 
-	if i.AddedAt.IsZero() {
-		return errors.Errorf("'added_at' timestamp is missing or not set")
-	}
+	i.AddedAt = time.Now().Format("2006-01-02")
 
 	i.AddedBy, err = normalizeMandatoryStrings(i.AddedBy)
 	if err != nil {
@@ -71,4 +86,36 @@ func normalizeMandatoryStrings(str string) (string, error) {
 	str = strings.Join(strs, " ")
 
 	return str, nil
+}
+
+type ListItemsData struct {
+	Items   []Item
+	Markets []string
+	Types   []string
+}
+
+func (lid *ListItemsData) GetMarkets() {
+	markets := []string{
+		Continente,
+		Belita,
+		PingoDoce,
+		Jumbo,
+		Lidl,
+	}
+
+	slices.Sort(markets)
+
+	lid.Markets = markets
+}
+
+func (lid *ListItemsData) GetTypes() {
+	types := []string{
+		Fruits,
+		Vegetables,
+		MeatAndFish,
+		Drinks,
+		Higiene,
+	}
+
+	lid.Types = types
 }
--- backend/internal/services/server.go
+++ backend/internal/services/server.go
@@ -1,11 +1,15 @@
 package services
 
 import (
+	"github.com/joaoferreiravnf/myShoppingApp.git/internal/models"
 	"github.com/joaoferreiravnf/myShoppingApp.git/internal/repository"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
+	"html/template"
+	"io"
 	"net/http"
+	"strconv"
 )
 
 type AppServer struct {
@@ -18,10 +22,26 @@ func NewAppServer(repo *repository.PostgresqlDb) *AppServer {
 	newEcho.Use(middleware.CORS())
 	newServer := &AppServer{newEcho, repo}
 
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+	newServer.Echo.Renderer = renderer
+
+	newEcho.Use(middleware.Logger())
+
 	itemGroup := newEcho.Group("/items")
 	itemGroup.GET("", newServer.ListItems)
+	itemGroup.POST("", newServer.CreateItem)
 
 	return newServer
+}
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func (s *AppServer) StartServer() {
@@ -34,5 +54,39 @@ func (s *AppServer) ListItems(c echo.Context) error {
 		return errors.Wrap(err, "error listing items from database")
 	}
 
-	return c.JSON(http.StatusOK, items)
+	listData := models.ListItemsData{
+		Items: items,
+	}
+
+	listData.GetTypes()
+	listData.GetMarkets()
+
+	return c.Render(http.StatusOK, "list_items.html", listData)
+}
+
+func (s *AppServer) CreateItem(c echo.Context) error {
+	quantity, err := strconv.Atoi(c.FormValue("quantity"))
+	if err != nil {
+		return errors.Wrap(err, "error converting quantity field to int")
+	}
+
+	newItem := models.Item{
+		Name:     c.FormValue("name"),
+		Quantity: quantity,
+		Type:     c.FormValue("type"),
+		Market:   c.FormValue("market"),
+		AddedBy:  c.FormValue("added_by"),
+	}
+
+	err = newItem.NormalizeFieldsForPersistence()
+	if err != nil {
+		return errors.Wrap(err, "error normalizing item fields for persistence")
+	}
+
+	err = s.repo.CreateItem(c.Request().Context(), newItem)
+	if err != nil {
+		return errors.Wrap(err, "error creating new item")
+	}
+
+	return c.Redirect(http.StatusSeeOther, "/items")
 }
